30115a68fd26bfe489eb78e8ecfae1e71425deef
haiyupeter <21haiyu21@163.com>
2012-06-09 20:16:36 +0800

    使用base64进行编码

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 // 王磊
+
+function init() {
+	// 默认页面，展现列表
+	showList();
+	document.addEventListener("deviceready", onDeviceReady, true);
+}
+var onDeviceReady = function() {
+	// 文件上传对象初始化
+	PictureUpload.init();
+}
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -18,16 +18,17 @@ var PictureUpload = {
         	destinationType=navigator.camera.DestinationType; 
 		},
 		capturePhoto : function() { 
-			navigator.camera.getPicture(PictureUpload.onPhotoURISuccess, PictureUpload.onFail, { quality: 50, destinationType: destinationType.FILE_URI });
+			navigator.camera.getPicture(PictureUpload.onPhotoURISuccess, PictureUpload.onFail, { quality: 50, destinationType: destinationType.DATA_URL });
 		},
 		selectPic : function() {
-			navigator.camera.getPicture(PictureUpload.onPhotoURISuccess, PictureUpload.onFail, { quality: 50,  destinationType: destinationType.FILE_URI, sourceType: pictureSource.PHOTOLIBRARY });
+			navigator.camera.getPicture(PictureUpload.onPhotoURISuccess, PictureUpload.onFail, { quality: 50,  destinationType: destinationType.DATA_URL, sourceType: pictureSource.PHOTOLIBRARY });
 		},
 		onPhotoURISuccess: function (imageURI) {  
 			window.location.hash="#upload";
     		var photoDiv = $('#photoDiv');  
-			$("#photoPath").val(imageURI);
-			$("#photoImg").attr('src',imageURI);
+			//$("#photoPath").val(imageURI);
+			$("#photoImg").attr('src', 'data:image/jpeg;base64,' + imageURI);
+			alert(imageURI);
 			if (UploadImgObj.imgSelectedCallback) {
 				UploadImgObj.imgSelectedCallback();
 			}
