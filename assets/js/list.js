f7cc9d425b18cfe61582660bc73aadcc6f61f72b
haiyupeter <21haiyu21@163.com>
2012-06-10 01:54:59 +0800

    列表页更新

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,4 +1,10 @@
-// 德模,haiyu
+// dmyang, haiyu
+
+function template(str, obj) {
+	return str.replace(/\{(\w+)\}/g, function(m) {
+		return obj[m.replace(/[{}]/g, '')];
+	});
+};
 
 function getPhotos(result) {
 	if (result.ret === -1) {
@@ -15,20 +21,29 @@ function getPhotos(result) {
 		$("#list").html("暂时没有数据，上传照片？<a href='#photo'>上传<a>");
 		return;				
 	}
+	var t = '\
+		<li>\
+			<img src="{img}" width="500px" height="360px" />\
+			<div class="img-info">\
+				<span class="img-title">{title}</span>\
+				<span class="img-comment">\
+					<span class="comment-btn"></span>\
+					<span class="comment-num">{comment}</span>\
+				</span>\
+			</div>\
+		</li>';
+		
 	for (var i = 0; i < data.length; i++) {
-		var itemHtml = "";
-		var desc = data[i].description;
-		var src = data[i].path;
-		var upload_time = data[i].upload_time;
-		if ( i % 2 == 0) {
-			itemHtml = '<div class="item_even"><ul><li><img src="' + src + '"></img></li><li class="text">' + upload_time + '<br/>' + desc + '</li></ul></div>';
-		} else {
-    		itemHtml = '<div class="item_odd"><ul><li class="text">' + upload_time + '<br/>' + desc + '</li><li><img src="' + src + '"></img></li></ul></div>';
-		}
-	    listHtml.push(itemHtml);
+		var d = data[i];
+		listHtml.push(template(t, {
+			img: d.path,
+			title: d.description,
+			comment: d.comment || 0
+		}));
 	}
-	$("#list").html(listHtml.join(''));
-}
+	
+	$("#list").append(listHtml.join(''));
+};
 
 var showList = function() {
 	//$("#content div").hide();
@@ -36,11 +51,12 @@ var showList = function() {
 	window.location.hash="#main";
 	var userId = CookieUtil.get("userid");
 	// 展现
-	ChildObj.getPhotoList(userId);
+	//ChildObj.getPhotoList(userId);$.getJSON('../data.json', function(d) {console.log(d)
+	getPhotos({'ret':'0', 'msg':'load success', 'data':[{'description':'出生了', 'path':'../img/591.jpg', 'comment':'10'},{'description':'很开心', 'path':'../img/588.jpg', 'comment':'10'},{'description':'今天小孩会坐起来玩玩具了', 'path':'../img/593.jpg', 'comment':'100'},{'description':'某一天，你也会很可爱，你是妈妈的幸福', 'path':'../img/595.jpg', 'comment':'5'},{'description':'开心开心', 'path':'../img/592.jpg', 'comment':'1200'},{'description':'开心了', 'path':'../img/599.jpg', 'comment':'15'}]});
 	/*
 	var userId = CookieUtil.get("userid");
 	$.getScript(webContext + "/ImageServlet?cmd=query&userId=" + userId).error(function(){
 		$("#list").html("服务器繁忙，请稍后重试！");
 	});
 	*/
-}
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,13 @@ function init() {
 	// TODO: 后续将其隐藏起来
 	showList();
 	document.addEventListener("deviceready", onDeviceReady, true);
-}
+	
+	var myScroll = new iScroll('wrapper', { hScrollbar: false, vScrollbar: false });
+
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+};
+
 var onDeviceReady = function() {
 	// 文件上传对象初始化
 	PictureUpload.init();
-}
+};
