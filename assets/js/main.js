cc6907a11ec9a1a11d70fe2533968f479f426e9d
betawang <ideal.l.wang@gmail.com>
2012-06-11 01:05:12 +0800

    again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 // 王磊
+
+function init() {
+	// 若用户已登录，默认页面，展现列表
+	// TODO: 后续将其隐藏起来
+	showList();
+	document.addEventListener("deviceready", onDeviceReady, true);
+}
+var onDeviceReady = function() {
+	// 文件上传对象初始化
+	PictureUpload.init();
+}
