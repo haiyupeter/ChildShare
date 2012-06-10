4c5241680e41f26c9a5e2b57492949a0afe722ee
haiyupeter <21haiyu21@163.com>
2012-06-10 11:13:13 +0800

    删除重复的代码

--- assets/js/user.js
+++ assets/js/user.js
@@ -76,7 +76,9 @@ var getUserInfo = function (result) {
 		CookieUtil.set('username', name);
 		$("#main h1").html(name + " - 亲子卷轴");
 		window.location.hash="#main";
-		showList();
+		//setTimeout(showList, 1000);
+		//console.log('aa');
+		// showList();
 	}
 }
 
