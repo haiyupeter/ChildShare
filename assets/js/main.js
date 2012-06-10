acad2d43f40f1d5326641b9ba78ccb976c80aad3
小网 <zwlijun@gmail.com>
2012-06-10 12:01:35 +0800

    update login

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,9 +113,17 @@ var onDeviceReady = function() {
 
     if(null != o){
         var login = E4M.getAnimation("login");
+        login.set("start", function(node, newClassName, styles){
+            var bg = document.querySelector(".login.bg");
+            var top = parseInt(document.defaultView.getComputedStyle(bg, null).top||-1, 10);
+
+            if(0 === top){
+                E4M.addClass(".login.form-panel", "on");
+            }
+        });
         login.set("stop", function(node, newClassName, styles){
             E4M.addClass(".login.form-panel", "on");
-        });      
+        });
         login.transition(".login.bg", "ease-up", {top:0});
     }
 })("login");
