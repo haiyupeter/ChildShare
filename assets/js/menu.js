f1cdf1d9ce0dcc8ab7f866a2a71591d845656642
betawang <ideal.l.wang@gmail.com>
2012-06-11 00:43:28 +0800

    main body implement

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,20 @@
-// 王磊
+//********************** 
+//author 王磊
+//create Date: 2012-06-09
+//**********************
+
+function initMenu(container, callback){
+	
+	var html = "";
+	
+	if(container){
+		container.innerHTML = html;
+	}
+	
+	container.on('onclick', function(evt){
+		var nodename = evt.getTarget();
+		if(typeof callback == "function"){
+			callback();
+		}
+	});
+}
